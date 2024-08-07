b1e5270cbf46ed5d2c251a245ebd3e722d7177d8
Ayush Nainwal <aayu@Ayushs-MacBook-Air.local>
2024-08-07 18:59:30 +0530

    Commented out metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,30 +9,30 @@ import SessionProvider from "../components/SessionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-	metadataBase: new URL("https://blog-breeze-nine.vercel.app/"),
+// export const metadata: Metadata = {
+// 	metadataBase: new URL("https://blog-breeze-nine.vercel.app/"),
 
-	title: {
-		template: "%s | Daily Blog",
-		default: "Blog Breeze",
-	},
-	authors: {
-		name: "chensokheng",
-	},
+// 	title: {
+// 		template: "%s | Daily Blog",
+// 		default: "Blog Breeze",
+// 	},
+// 	authors: {
+// 		name: "chensokheng",
+// 	},
 
-	description:
-		"Explore a world of captivating stories and insightful articles on our blog. From the latest trends to in-depth analyses, our blog covers a wide range of topics to keep you informed and entertained. Join our community of readers and discover thought-provoking content that sparks curiosity and fosters discussion. Stay updated with our diverse collection of blog posts, written by passionate contributors who share their expertise and unique perspectives. Engage with a platform that goes beyond the ordinary, providing you with enriching content that resonates with your interests.",
-	openGraph: {
-		title: "Daily Blog",
-		description:
-			"Explore a world of captivating stories and insightful articles on our blog. From the latest trends to in-depth analyses, our blog covers a wide range of topics to keep you informed and entertained. Join our community of readers and discover thought-provoking content that sparks curiosity and fosters discussion. Stay updated with our diverse collection of blog posts, written by passionate contributors who share their expertise and unique perspectives. Engage with a platform that goes beyond the ordinary, providing you with enriching content that resonates with your interests.",
-		url: "https://blog-breeze-nine.vercel.app/",
-		siteName: "Blog Breeze",
-		images: "/og.png",
-		type: "website",
-	},
-	keywords: ["Ayush Nainwal", "ayushn2", "ayushn2"],
-};
+// 	description:
+// 		"Explore a world of captivating stories and insightful articles on our blog. From the latest trends to in-depth analyses, our blog covers a wide range of topics to keep you informed and entertained. Join our community of readers and discover thought-provoking content that sparks curiosity and fosters discussion. Stay updated with our diverse collection of blog posts, written by passionate contributors who share their expertise and unique perspectives. Engage with a platform that goes beyond the ordinary, providing you with enriching content that resonates with your interests.",
+// 	openGraph: {
+// 		title: "Daily Blog",
+// 		description:
+// 			"Explore a world of captivating stories and insightful articles on our blog. From the latest trends to in-depth analyses, our blog covers a wide range of topics to keep you informed and entertained. Join our community of readers and discover thought-provoking content that sparks curiosity and fosters discussion. Stay updated with our diverse collection of blog posts, written by passionate contributors who share their expertise and unique perspectives. Engage with a platform that goes beyond the ordinary, providing you with enriching content that resonates with your interests.",
+// 		url: "https://blog-breeze-nine.vercel.app/",
+// 		siteName: "Blog Breeze",
+// 		images: "/og.png",
+// 		type: "website",
+// 	},
+// 	keywords: ["Ayush Nainwal", "ayushn2", "ayushn2"],
+// };
 
 export default function RootLayout({
 	children,
